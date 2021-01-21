f74cca0677f32c0d3bbb39ecb5a29b987b9cd573
brauliomarcalupo <58892312+brauliomarcalupo@users.noreply.github.com>
2021-01-21 18:18:26 -0500

    ingreso de id dinámico
    se añadió un ingreso por teclado para poder escoger diversos usuarios  y no uno solo de forma estática

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 # Ingresar el id del usuario en user_id.
 # bin_url contiene el url directo a los usuarios.
-user_id = '22656'
+print("Ingrese un Id: ")
+user_id = input()
 bin_url = f'http://stackoverflow.com/users/{user_id}?tab=tags'
 
 # Se inicializa una variable que esta lista para hacer llamadas por protocolo http
